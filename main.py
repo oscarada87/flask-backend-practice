664447354bb38609014929f2c4e40186a92764fd
Oscar <oscarada87@gmail.com>
2018-11-01 22:36:37 +0800

    '消掉註解'

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def index():
         test.Start()
         news = test.GetNews()
         return str(news)
-
-# 判斷自己執行非被當做引入的模組，因為 __name__ 這變數若被當做模組引入使用就不會是 __main__
+        
 if __name__ == '__main__':
     app.run()

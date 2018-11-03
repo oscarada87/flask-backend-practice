cb9b6946450c325c7eb977b5768a8a76a1a247f8
Oscar <oscarada87@gmail.com>
2018-11-03 22:32:13 +0800

    153生日弄些更好玩的東西

--- main.py
+++ main.py
@@ -21,12 +21,23 @@ def birthday():
 
 @app.route('/')
 def index():
-    text = "歡迎光臨!!"
+    text = """歡迎光臨!!
+              遊戲玩法:
+              1. 在網址列後輸入各種英文單字即可切換到另一個頁面
+              EX: https://flask-practice-crawler.herokuapp.com/test
+              2. 某些頁面需要密碼才會提供獎勵，密碼測試方式請在網址後面加上 "key='你的密碼'"
+                 因為沒時間寫前端，所以就將就一點吧
+              EX: https://flask-practice-crawler.herokuapp.com/test?key=簡子融                
+    """
     return text
 
 @app.route('/EasterEgg')
-def test():
+def EasterEgg():
     return render_template('gallary.html')
+
+@app.route('/test')
+def test():
+    return render_template('resume.html')
 
 if __name__ == '__main__':
     app.run()
